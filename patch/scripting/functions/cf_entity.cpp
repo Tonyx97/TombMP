51764d6e309f88455acf0c8592f368a1d48f80e8
Tony Pérez <tonyx97@hotmail.es>
2021-11-07 01:08:56 +0100

    Update cf_entity.cpp

--- patch/scripting/functions/cf_entity.cpp
+++ patch/scripting/functions/cf_entity.cpp
@@ -26,8 +26,8 @@ void cf_entity::register_functions(sol::state* vm)
 	vm->set_function("isEntity", [&](game_entity* entity) { return g_level->has_entity(entity); });
 	vm->set_function("asPlayer", [&](game_entity_base* base) { return utils::rtti::safe_cast<game_player>(base); });
 	vm->set_function("isPlayer", [&](game_player* player) { return g_level->has_player(player); });
-	vm->set_function("destroyEntity", [&](game_entity_base* entity) { KillItem(entity->get_item_id()); });
-	vm->set_function("destroyEntity", [&](ITEM_INFO* item)
+	vm->set_function("destroyEntity", [&](game_entity* entity) { KillItem(entity->get_item_id()); });
+	vm->set_function("destroyEntityFromItem", [&](ITEM_INFO* item)
 	{
 		if (auto entity = g_level->get_entity_by_item(item))
 			KillItem(entity->get_item_id());
