17daa981ff57c6fcdba54064438c95e1fb5185ee
Tony Pérez <tonyx97@hotmail.es>
2021-11-09 01:41:32 +0100

    Added getAnimFrameBase.

--- patch/scripting/functions/cf_anim.cpp
+++ patch/scripting/functions/cf_anim.cpp
@@ -9,11 +9,17 @@
 #include "cf_defs.h"
 
 #include <specific/file.h>
+#include <game/laraanim.h>
 
 void cf_anim::register_functions(sol::state* vm)
 {
 	vm->set_function("loadAnimation", [&](const char* filename)
 	{
 		return load_animation(filename);
+	});
+
+	vm->set_function("getAnimFrameBase", [&](int16_t anim, int16_t offset) -> int16_t
+	{
+		return GF(anim, offset);
 	});
 }
