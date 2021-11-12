b1e260107216bc94eff91ceba7bc74b5909abede
Tony Pérez <tonyx97@hotmail.es>
2021-11-12 23:01:29 +0100

    trash

--- obj_exporter/main.cpp
+++ obj_exporter/main.cpp
@@ -1,6 +1,9 @@
 #include <windows.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <stdio.h>
 
 struct tr_model  // 18 bytes
 {
@@ -84,6 +87,16 @@ struct OBJECT_INFO
 	uint16_t water_creature;	// is this is water based baddie? needed for SFX in shallow water
 };
 
+struct PHDTEXTURESTRUCT
+{
+	uint16_t drawtype,
+			 tpage,
+			 u1, v1,
+			 u2, v2,
+			 u3, v3,
+			 u4, v4;
+};
+
 std::ifstream g_level;
 std::ofstream g_out_obj;
 
@@ -112,6 +125,94 @@ void write(void* data, int size)
 {
 	g_out_obj.write((char*)data, size);
 }
+const int BYTES_PER_PIXEL = 3; /// red, green, & blue
+const int FILE_HEADER_SIZE = 14;
+const int INFO_HEADER_SIZE = 40;
+
+void generateBitmapImage(unsigned char* image, int height, int width, char* imageFileName);
+unsigned char* createBitmapFileHeader(int height, int stride);
+unsigned char* createBitmapInfoHeader(int height, int width);
+
+void generateBitmapImage(unsigned char* image, int width, int height, char* imageFileName)
+{
+	int widthInBytes = width * BYTES_PER_PIXEL;
+
+	unsigned char padding[3] = { 0, 0, 0 };
+	int paddingSize = (4 - (widthInBytes) % 4) % 4;
+
+	int stride = (widthInBytes)+paddingSize;
+
+	FILE* imageFile;
+	
+	fopen_s(&imageFile, imageFileName, "wb");
+
+	unsigned char* fileHeader = createBitmapFileHeader(height, stride);
+	fwrite(fileHeader, 1, FILE_HEADER_SIZE, imageFile);
+
+	unsigned char* infoHeader = createBitmapInfoHeader(height, width);
+	fwrite(infoHeader, 1, INFO_HEADER_SIZE, imageFile);
+
+	int i;
+	for (i = 0; i < height; i++) {
+		fwrite(image + (i * widthInBytes), BYTES_PER_PIXEL, width, imageFile);
+		fwrite(padding, 1, paddingSize, imageFile);
+	}
+
+	fclose(imageFile);
+}
+
+unsigned char* createBitmapFileHeader(int height, int stride)
+{
+	int fileSize = FILE_HEADER_SIZE + INFO_HEADER_SIZE + (stride * height);
+
+	static unsigned char fileHeader[] = {
+		0,0,     /// signature
+		0,0,0,0, /// image file size in bytes
+		0,0,0,0, /// reserved
+		0,0,0,0, /// start of pixel array
+	}; 
+	fileHeader[0] = (unsigned char)('B');
+	fileHeader[1] = (unsigned char)('M');
+	fileHeader[2] = (unsigned char)(fileSize);
+	fileHeader[3] = (unsigned char)(fileSize >> 8);
+	fileHeader[4] = (unsigned char)(fileSize >> 16);
+	fileHeader[5] = (unsigned char)(fileSize >> 24);
+	fileHeader[10] = (unsigned char)(FILE_HEADER_SIZE + INFO_HEADER_SIZE);
+
+	return fileHeader;
+}
+
+unsigned char* createBitmapInfoHeader(int height, int width)
+{
+	static unsigned char infoHeader[] = {
+		0,0,0,0, /// header size
+		0,0,0,0, /// image width
+		0,0,0,0, /// image height
+		0,0,     /// number of color planes
+		0,0,     /// bits per pixel
+		0,0,0,0, /// compression
+		0,0,0,0, /// image size
+		0,0,0,0, /// horizontal resolution
+		0,0,0,0, /// vertical resolution
+		0,0,0,0, /// colors in color table
+		0,0,0,0, /// important color count
+	};
+
+	infoHeader[0] = (unsigned char)(INFO_HEADER_SIZE);
+	infoHeader[4] = (unsigned char)(width);
+	infoHeader[5] = (unsigned char)(width >> 8);
+	infoHeader[6] = (unsigned char)(width >> 16);
+	infoHeader[7] = (unsigned char)(width >> 24);
+	infoHeader[8] = (unsigned char)(height);
+	infoHeader[9] = (unsigned char)(height >> 8);
+	infoHeader[10] = (unsigned char)(height >> 16);
+	infoHeader[11] = (unsigned char)(height >> 24);
+	infoHeader[12] = (unsigned char)(1);
+	infoHeader[14] = (unsigned char)(BYTES_PER_PIXEL * 8);
+
+	return infoHeader;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -126,7 +227,10 @@ int main(int argc, char** argv)
 	auto num_textiles = read<int32_t>();
 
 	g_offset += num_textiles * 65536;	// Textile8
-	g_offset += num_textiles * 131072;	// Textile16
+
+	auto texture_pages = new uint8_t[num_textiles * 131072]();
+
+	read<uint8_t>(texture_pages, num_textiles * 131072);
 
 	g_offset += 4;	// Unused
 
@@ -256,6 +360,67 @@ int main(int argc, char** argv)
 		obj->loaded = 1;
 	}
 
+	// IGNORE STATIC OBJS
+
+	auto NumSpriteTextures = read<int32_t>();
+
+	g_offset += NumSpriteTextures * 32;	// StaticMeshes
+
+	// IGNORE SPRITES
+
+	auto NumStaticMeshes = read<int32_t>();
+
+	g_offset += NumStaticMeshes * 16;	// SpriteTextures
+
+	// IGNORE DYNAMIC SPRITES
+
+	auto NumSpriteSequences = read<int32_t>();
+
+	g_offset += NumSpriteSequences * 8;	// SpriteSequences
+
+	// IGNORE CAMERAS
+
+	auto NumCameras = read<int32_t>();
+
+	g_offset += NumCameras * 16;	// Cameras
+
+	// IGNORE SOUNDS
+
+	auto NumSoundSources = read<int32_t>();
+
+	g_offset += NumSoundSources * 16;	// SoundSources
+
+	// IGNORE BOXES
+
+	auto NumBoxes = read<int32_t>();
+
+	g_offset += NumBoxes * 8;	// Boxes
+
+	// IGNORE OVERLAPS
+
+	auto NumOverlaps = read<int32_t>();
+
+	g_offset += NumOverlaps * 2;	// Overlaps
+
+	// IGNORE OVERLAPS
+
+	g_offset += NumBoxes * 20;		// Zones
+
+	// IGNORE ANIMATED TEXTURES
+
+	auto NumAnimatedTextures = read<int32_t>();
+
+	g_offset += NumAnimatedTextures * 2;	// AnimatedTextures
+
+	// OBJECT TEXTURES INFO
+
+	auto NumObjectTextures = read<int32_t>();
+	auto text_info = new PHDTEXTURESTRUCT[NumObjectTextures]();
+
+	read<PHDTEXTURESTRUCT>(text_info, NumObjectTextures);
+
+	// adjust meshes pointers
+
 	for (int i = 0; i < NumMeshPointers; ++i)
 		meshes[i] = meshes_base + (int)meshes[i] / 2;
 
@@ -298,9 +463,162 @@ int main(int argc, char** argv)
 
 		g_out_obj = std::ofstream("..\\patch\\" + obj_name, std::ios::binary | std::ios::trunc);
 
-		obj_id = 353;
+		obj_id = 232;
 
 		auto obj = &objects[obj_id];
+
+		int generated_textures = 0;
+
+		/*for (int i = 0; i < num_textiles; ++i)
+		{
+			std::vector<uint8_t> page_data;
+
+			auto page_base = texture_pages + (i * 256 * 256 * 2);
+
+			for (int y = 0; y < 256; ++y)
+				for (int x = 0; x < 256; ++x)
+				{
+					auto pixel = (int16_t*)page_base + x + (256 - y) * 256;
+
+					uint8_t r = (*pixel >> 7) & 0xf8;
+					uint8_t g = (*pixel >> 2) & 0xf8;
+					uint8_t b = (*pixel << 3) & 0xf8;
+
+					page_data.push_back(b);
+					page_data.push_back(g);
+					page_data.push_back(r);
+				}
+
+			generateBitmapImage((unsigned char*)page_data.data(), 256, 256, (char*)("pages\\page_" + std::to_string(generated_textures++) + ".bmp").c_str());
+		}*/
+
+		float UVTable[65536];
+
+		for (int i = 0; i < 65536; ++i)
+			UVTable[i] = (float)(i + 1) * (1.f / 65536.f);
+
+		auto get_texture_page = [&](int page)
+		{
+			return (int16_t*)texture_pages + (page * 256 * 256);
+		};
+
+		auto get_texture_part = [&](int16_t* page, int offset)
+		{
+			return &page[offset];
+		};
+
+		auto fix_and_gen_texture_part = [&](int16_t* face, bool gt4 = true)
+		{
+			auto texture_info = text_info + face[gt4 ? 4 : 3];
+			auto page = get_texture_page(texture_info->tpage);
+
+			auto u1_off = texture_info->u1,
+				 v1_off = texture_info->v1,
+				 u2_off = texture_info->u2,
+				 v2_off = texture_info->v2,
+				 u3_off = texture_info->u3,
+				 v3_off = texture_info->v3,
+				 u4_off = texture_info->u4,
+				 v4_off = texture_info->v4;
+
+			int16_t u1 = (u1_off / 256),
+					v1 = (v1_off / 256),
+					u2 = (u2_off / 256),
+					v2 = (v2_off / 256),
+					u3 = (u3_off / 256),
+					v3 = (v3_off / 256),
+					u4 = (u4_off / 256),
+					v4 = (v4_off / 256);
+
+			std::cout << "page(" << texture_info->tpage << ") "
+					  << "u1(" << u1 << ") v1(" << v1 << ") | "
+					  << "u2(" << u2 << ") v2(" << v2 << ") | "
+					  << "u3(" << u3 << ") v3(" << v3 << ") | "
+					  << "u4(" << u4 << ") v4(" << v4 << ")" << std::endl;
+
+			int width = std::abs(u1 - u2),
+				height = std::abs(v1 - v4);
+
+			auto tu = UVTable[u1_off];
+			auto tv = UVTable[v1_off];
+
+			std::cout << "width: " << width << " height: " << height << std::endl;
+
+			auto texture_start = get_texture_part(page, 0);
+
+			std::vector<uint8_t> face_texture;
+
+			int sx = 256,
+				sy = 256;
+
+			{
+				auto row_id = u1;
+				auto column_id = v1 + 3;
+
+				auto row_start = u1_off & ~255;
+				auto col_start = v1_off & ~255;
+
+				auto pixel = &page[0];
+
+				for (int i = 0; i < 256; ++i)
+				{
+					for (int j = 0; j < 256; ++j)
+					{
+						//pixel = &page[-(row_id * 256) - (column_id * 256) + j + i * 256];
+
+						uint8_t r = (*pixel >> 7) & 0xf8;
+						uint8_t g = (*pixel >> 2) & 0xf8;
+						uint8_t b = (*pixel << 3) & 0xf8;
+
+						face_texture.push_back(b);
+						face_texture.push_back(g);
+						face_texture.push_back(r);
+
+						++pixel;
+					}
+
+					//pixel += 256;
+				}
+			}
+
+			/*{
+				for (int y = 256 - 1; y >= 0; --y)
+				//for (int y = 0; y < 256; ++y)
+					for (int x = 256 - 1; x >= 0; --x)
+					//for (int x = 0; x < 256; ++x)
+					{
+						auto pixel = texture_start + x + y * 256;
+
+						uint8_t r = (*pixel >> 7) & 0xf8;
+						uint8_t g = (*pixel >> 2) & 0xf8;
+						uint8_t b = (*pixel << 3) & 0xf8;
+
+						face_texture.push_back(b);
+						face_texture.push_back(g);
+						face_texture.push_back(r);
+					}
+			}*/
+
+			generateBitmapImage((unsigned char*)face_texture.data(), sx, sy, (char*)("mesh_" + std::to_string(generated_textures++) + ".bmp").c_str());
+
+			/*{
+				for (int y = 256 - 1; y >= 0; --y)
+					for (int x = 0; x < 256; ++x)
+					{
+						auto pixel = texture_start + x + y * 256;
+
+						uint8_t r = (*pixel >> 7) & 0xf8;
+						uint8_t g = (*pixel >> 2) & 0xf8;
+						uint8_t b = (*pixel << 3) & 0xf8;
+
+						face_texture.push_back(b);
+						face_texture.push_back(g);
+						face_texture.push_back(r);
+					}
+
+				generateBitmapImage((unsigned char*)face_texture.data(), 256, 256, (char*)("test_mesh_" + std::to_string(generated_textures++) + ".bmp").c_str());
+			}*/
+		};
 
 		auto process_mesh_array = [&](int16_t* mesh_ptr, int32_t* mesh_size)
 		{
@@ -319,10 +637,12 @@ int main(int argc, char** argv)
 
 			// reset faces texture info
 
-			for (auto face = gt4_faces; face < gt4_faces + gt4_faces_count * 5; face += 5) face[4] = 0;
-			for (auto face = gt3_faces; face < gt3_faces + gt3_faces_count * 4; face += 4) face[3] = 0;
-			for (auto face = g4_faces; face < g4_faces + g4_faces_count * 5; face += 5)	face[4] = 0;
-			for (auto face = g3_faces; face < g3_faces + g3_faces_count * 4; face += 4)	face[3] = 0;
+			int gt4_pixels_count = 0;
+
+			for (auto face = gt4_faces; face < gt4_faces + gt4_faces_count * 5; face += 5)	fix_and_gen_texture_part(face);
+			for (auto face = gt3_faces; face < gt3_faces + gt3_faces_count * 4; face += 4)	fix_and_gen_texture_part(face, false);
+			for (auto face = g4_faces; face < g4_faces + g4_faces_count * 5; face += 5)		fix_and_gen_texture_part(face);
+			for (auto face = g3_faces; face < g3_faces + g3_faces_count * 4; face += 4)		fix_and_gen_texture_part(face, false);
 
 			auto size = *mesh_size = (curr_ptr - mesh_ptr) * sizeof(int16_t);
 			auto mesh_data = new uint8_t[size]();
@@ -369,8 +689,11 @@ int main(int argc, char** argv)
 		std::cout << "Object " << obj_id << " exported to file " << obj_name << std::endl;
 		std::cout << "Export more? (y/n)" << std::endl;
 
-		std::cin >> option;
+		//std::cin >> option;
+		break;
 	}
+
+	ShellExecuteA(nullptr, nullptr, "mesh_2.bmp", nullptr, nullptr, SW_HIDE);
 
 	// gl to free all the pools :risitas: (let windows kernel do its job)
 
